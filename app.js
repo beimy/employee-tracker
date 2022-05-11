1b027abad6096b47a17bf95094a490d56ef8a056
Beimy <arbm159357@gmail.com>
2022-05-11 17:13:31 -0400

    removed express, set up all queiries for getting, and adding to tables

---
+++ app.js
@@ -0,0 +1,88 @@
+const res = require('express/lib/response');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Connecting to Database
+const db = mysql.createConnection(
+    {
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database
+    },
+
+    console.log(`Connected to the ${process.env.database} database`)
+);
+
+// DB Queries
+
+// Return all data from a table
+function getAllDepartments() {
+    db.query(`SELECT * FROM department`, (err, rows) => {
+        console.log(rows);
+    });
+}
+
+function getAllRoles() {
+    db.query(`SELECT * FROM role`, (err, rows) => {
+        console.log(rows);
+    });
+}
+
+function getAllEmployees() {
+    db.query(`SELECT * FROM employee`, (err, rows) => {
+        console.log(rows);
+    });
+}
+
+
+// Insert into tables
+function addDepartment(newDepartmentName) {
+    const sql = `INSERT INTO department (name)
+                 VALUES (?)`;
+    const params = newDepartmentName;
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(result);
+        return result;
+    });
+};
+
+function addRole( newTitle, newSalary) {
+    const sql = `INSERT INTO role (title, salary)
+                 VALUES (?,?)`;
+    const params = [newTitle, newSalary];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(result);
+        return result;
+    });
+};
+
+function addEmployee( newFirst_name, newLast_name, newRole, newManagerId ) {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                 VALUES (?,?,?,?)`;
+    const params = [newFirst_name, newLast_name, newRole, newManagerId];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(result);
+        return result;
+    });
+};
+
+
+
+
+
+
+
+
